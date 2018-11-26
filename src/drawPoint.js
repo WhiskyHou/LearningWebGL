39c3e6d0562008ada688b82523bb39ccb00fb4ee
whiskyHou_Rog <ghostgaz@126.com>
2018-11-26 21:48:46 +0800

    point1

--- src/drawPoint.js
+++ src/drawPoint.js
@@ -1,12 +1,12 @@
 let VSHADER =
     'void main() {\n' +
     '   gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' +
-    '   gl_PointSize = 25.0;\n' +
+    '   gl_PointSize = 10.0;\n' +
     '}\n';
 
 let FSHADER =
     'void main() {\n' +
-    '   gl_FragColor = vec4(1.0, 0.3, 0.5, 1.0);\n' +
+    '   gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n' +
     '}\n';
 
 function main() {
